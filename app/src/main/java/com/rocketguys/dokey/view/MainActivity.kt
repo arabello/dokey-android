2dddc0607ff37a1937c1493f8e975d78fa0ed040
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2018-08-19 17:36:16 +0200

    Add color to selected icon in BottomNavigation menu

--- app/src/main/java/com/rocketguys/dokey/view/MainActivity.kt
+++ app/src/main/java/com/rocketguys/dokey/view/MainActivity.kt
@@ -9,22 +9,32 @@ import kotlinx.android.synthetic.main.activity_home.*
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        if (item.itemId != R.id.navigation_more)
+        with(navigation.menu){
+            findItem(R.id.navigation_home).setIcon(R.drawable.ic_outline_grid_on)
+            findItem(R.id.navigation_shortcut).setIcon(R.drawable.ic_outline_keyboard)
+            findItem(R.id.navigation_system).setIcon(R.drawable.ic_outline_computer)
+        }
+
         when (item.itemId) {
             R.id.navigation_home -> {
                 message.setText(R.string.title_home)
+                item.setIcon(R.drawable.ic_outline_grid_on_grad_1)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_shortcut -> {
                 message.setText(R.string.title_shortcut)
+                item.setIcon(R.drawable.ic_outline_keyboard_grad_2)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_system -> {
                 message.setText(R.string.title_system)
+                item.setIcon(R.drawable.ic_outline_computer_grad_3)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_more -> {
-                message.setText(R.string.title_more)
-                return@OnNavigationItemSelectedListener true
+                return@OnNavigationItemSelectedListener false
             }
         }
         false
@@ -35,5 +45,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.itemIconTintList = null
     }
 }
