1c8f9ceedfbecac7c61cf77c3c3d8cf088825d84
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2018-08-19 18:29:54 +0200

    Style and init BottomNavigationView

--- app/src/main/java/com/rocketguys/dokey/view/HomeActivity.kt
+++ app/src/main/java/com/rocketguys/dokey/view/HomeActivity.kt
@@ -5,32 +5,35 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.rocketguys.dokey.R
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.toolbar.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
 
-        if (item.itemId != R.id.navigation_more)
+        if (item.itemId == R.id.navigation_more)
+            return@OnNavigationItemSelectedListener false
+
         with(navigation.menu){
-            findItem(R.id.navigation_home).setIcon(R.drawable.ic_outline_grid_on)
+            findItem(R.id.navigation_launchpad).setIcon(R.drawable.ic_outline_grid_on)
             findItem(R.id.navigation_shortcut).setIcon(R.drawable.ic_outline_keyboard)
             findItem(R.id.navigation_system).setIcon(R.drawable.ic_outline_computer)
         }
 
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_launchpad -> {
                 item.setIcon(R.drawable.ic_outline_grid_on_grad_1)
+                toolbar.setTitle(R.string.title_launchpad)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_shortcut -> {
-                message.setText(R.string.title_shortcut)
                 item.setIcon(R.drawable.ic_outline_keyboard_grad_2)
+                // TODO toolbar.setTitle(focusedApp.getTitle())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_system -> {
-                message.setText(R.string.title_system)
                 item.setIcon(R.drawable.ic_outline_computer_grad_3)
+                toolbar.setTitle(R.string.title_system)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_more -> {
@@ -43,8 +46,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
 
+        // Init BottomNavigationView
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         navigation.itemIconTintList = null
+        navigation.selectedItemId = R.id.navigation_launchpad
+        toolbar.setTitle(R.string.title_launchpad)
     }
 }
