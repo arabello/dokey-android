d23258bcb59b7643c3671dc8672fad76db50be4d
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2018-08-21 18:24:37 +0200

    Add action lock view

--- app/src/main/java/io/rocketguys/dokey/view/HomeActivity.kt
+++ app/src/main/java/io/rocketguys/dokey/view/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
@@ -27,6 +28,46 @@ class HomeActivity : AppCompatActivity() {
 
     val mActiveAppAdapter = ActiveAppAdapter(ArrayList())
 
+    enum class LOCK{ INVISIBLE, CLOSE, OPEN}
+    var lockState = LOCK.CLOSE
+
+    private fun MenuItem.transIconTo(newIcon: Drawable, duration: Int){
+        val bgTrans = TransitionDrawable(arrayOf(this.icon, newIcon))
+        bgTrans.isCrossFadeEnabled = true
+        this.icon = bgTrans
+        bgTrans.startTransition(duration)
+    }
+
+    private fun MenuItem.transStateTo(newState: LOCK, duration: Int){
+        if (this.itemId != R.id.action_lock)
+            return
+
+        lateinit var trans: TransitionDrawable
+
+        when(newState){
+            HomeActivity.LOCK.INVISIBLE -> {
+                trans = TransitionDrawable(arrayOf(this.icon, ColorDrawable(Color.TRANSPARENT)))
+                Handler().postDelayed({
+                    runOnUiThread {
+                        this.isVisible = false
+                    }
+                }, DRAWABLE_GRAD_TRANS_DURATION.toLong())
+            }
+            HomeActivity.LOCK.CLOSE -> {
+                this.isVisible = true
+                trans = TransitionDrawable(arrayOf(this.icon, ContextCompat.getDrawable(baseContext, R.drawable.ic_lock_grad_2)))
+            }
+            HomeActivity.LOCK.OPEN -> {
+                this.isVisible = true
+                trans = TransitionDrawable(arrayOf(this.icon, ContextCompat.getDrawable(baseContext, R.drawable.ic_lock_open_grad_2)))
+            }
+        }
+
+        trans.isCrossFadeEnabled = true
+        this.icon = trans
+        trans.startTransition(duration)
+    }
+
     private fun RecyclerView.transBackgroundTo(newBackground: Drawable, duration: Int){
         val start = if (this.background == null) ColorDrawable(Color.TRANSPARENT) else this.background
         val bgTrans = TransitionDrawable(arrayOf(start, newBackground))
@@ -35,21 +76,21 @@ class HomeActivity : AppCompatActivity() {
         bgTrans.startTransition(duration)
     }
 
-    private fun MenuItem.transIconTo(newIcon: Drawable, duration: Int){
-        val trans = TransitionDrawable(arrayOf(this.icon, newIcon))
-        trans.isCrossFadeEnabled = true
-        this.icon = trans
-        trans.startTransition(duration)
-    }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        toolbar.inflateMenu(R.menu.toolbar_launchpad)
+        toolbar.inflateMenu(R.menu.toolbar)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_edit -> {
             Toast.makeText(baseContext, "action_edit", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.action_lock -> {
+            lockState = if (lockState == LOCK.OPEN) LOCK.CLOSE else LOCK.OPEN
+            item.transStateTo(lockState, DRAWABLE_GRAD_TRANS_DURATION)
+            Toast.makeText(baseContext, "action_lock $lockState", Toast.LENGTH_SHORT).show()
             true
         }
 
@@ -73,27 +114,49 @@ class HomeActivity : AppCompatActivity() {
 
         when (item.itemId) {
             R.id.navigation_launchpad -> {
-                item.setIcon(R.drawable.ic_outline_grid_on_grad_1)
-                toolbar.setTitle(R.string.title_launchpad)
+                // Active apps
                 recyclerView?.transBackgroundTo(ContextCompat.getDrawable(baseContext, R.color.grad_1)!!, DRAWABLE_GRAD_TRANS_DURATION)
+
+                // NavigationBottomView
+                item.setIcon(R.drawable.ic_outline_grid_on_grad_1)
+
+                // Toolbar
+                toolbar.setTitle(R.string.title_launchpad)
                 toolbar.menu.findItem(R.id.action_edit)?.transIconTo(ContextCompat.getDrawable(baseContext, R.drawable.ic_edit_grad_1)!!, DRAWABLE_GRAD_TRANS_DURATION)
+                toolbar.menu.findItem(R.id.action_lock)?.transStateTo(LOCK.INVISIBLE, DRAWABLE_GRAD_TRANS_DURATION)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_shortcut -> {
-                item.setIcon(R.drawable.ic_outline_keyboard_grad_2)
-                // TODO toolbar.setTitle(focusedApp.getTitle())
+                // Active apps
                 recyclerView?.transBackgroundTo(ContextCompat.getDrawable(baseContext, R.color.grad_2)!!, DRAWABLE_GRAD_TRANS_DURATION)
+
+                // NavigationBottomView
+                item.setIcon(R.drawable.ic_outline_keyboard_grad_2)
+
+                // Toolbar
+                // TODO toolbar.setTitle(focusedApp.getTitle())
                 toolbar.menu.findItem(R.id.action_edit)?.transIconTo(ContextCompat.getDrawable(baseContext, R.drawable.ic_edit_grad_2)!!, DRAWABLE_GRAD_TRANS_DURATION)
+                toolbar.menu.findItem(R.id.action_lock)?.transStateTo(lockState, DRAWABLE_GRAD_TRANS_DURATION)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_system -> {
-                item.setIcon(R.drawable.ic_outline_computer_grad_3)
-                toolbar.setTitle(R.string.title_system)
+                // Active apps
                 recyclerView?.transBackgroundTo(ContextCompat.getDrawable(baseContext, R.color.grad_3)!!, DRAWABLE_GRAD_TRANS_DURATION)
+
+                // NavigationBottomView
+                item.setIcon(R.drawable.ic_outline_computer_grad_3)
+
+                // Toolbar
+                toolbar.setTitle(R.string.title_system)
                 toolbar.menu.findItem(R.id.action_edit)?.transIconTo(ContextCompat.getDrawable(baseContext, R.drawable.ic_edit_grad_3)!!, DRAWABLE_GRAD_TRANS_DURATION)
+                toolbar.menu.findItem(R.id.action_lock)?.transStateTo(LOCK.INVISIBLE, DRAWABLE_GRAD_TRANS_DURATION)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_more -> {
+
                 return@OnNavigationItemSelectedListener false
             }
         }
